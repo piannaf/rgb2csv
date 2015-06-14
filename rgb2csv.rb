4f8ae5bde1bb7a539bc3dd67fe29899710ee0219
Justin Mancinelli <justin.mancinelli@gmail.com>
2015-06-14 11:33:30 +0000

    refactor

--- rgb2csv.rb
+++ rgb2csv.rb
@@ -1,27 +1,43 @@
 require 'tabula'
 require 'pp'
 
-pdf_file = ARGV.first
+def extract_data_from(pdf_file)
+  extractor = Tabula::Extraction::ObjectExtractor.new(pdf_file, 1..2 )
+  data = extractor.extract.map { |page|
+    table_area = select_table_area_from(page)
+    table = extract_table_from(table_area)
+    cleanup(table)
+  }
+  extractor.close!
 
-extractor = Tabula::Extraction::ObjectExtractor.new(pdf_file, 1..2 )
-extractor.extract.each do |page|
-  page_top = 30 # Ignore page header
-  page_left = 0
-  page_bottom = page.height-40 # Ignore page footer
-  page_right = page.width
+  data
+end
 
-  page_area = [page_top, page_left, page_bottom, page_right]
+def select_table_area_from(page)
+  table_top = 30 # Ignore page header
+  table_left = 0
+  table_bottom = page.height-40 # Ignore page footer
+  table_right = page.width
+
+  table_area = [table_top, table_left, table_bottom, table_right]
+  page.get_area(table_area)
+end
+
+def extract_table_from(table_area)
   vertical_rulings = [50,103,198,245,280,370,400,450,542,630,703,727].map{ |n|
     Tabula::Ruling.new(0, n, 0, 1000)
   }
 
-  rows = page.get_area(page_area)
-             .get_table(:vertical_rulings => vertical_rulings)
-             .rows
-             .map {|row|
-               # extract text from text elements
-               row.map { |te| te.text.gsub(/\s+/, '').strip }
-             }
-  pp rows.last(2)
+  table_area.get_table(:vertical_rulings => vertical_rulings)
 end
-extractor.close!
+
+def cleanup(table)
+  table.rows.map {|row|
+    # extract text from text elements
+    # and remove some strange whitespace
+    row.map { |te| te.text.gsub(/\s+/, '').strip }
+  }
+end
+
+pdf_file = ARGV.first
+pp extract_data_from(pdf_file)
